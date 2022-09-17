6a795756fe3be9b1ce606409b6ea8575f48a936e
Cristian <cristian.rojas@live.fr>
2022-09-17 22:50:03 +0200

    Delete currentUser getting methods

--- PersonalDashboard/Screens/Toggl/TogglUserDbApi.swift
+++ PersonalDashboard/Screens/Toggl/TogglUserDbApi.swift
@@ -16,39 +16,11 @@ extension TogglUser {
     user?.lastSync = Date.now
     try viewContext.save()
   }
-  
-  static func saveCurrentUserSyncDate(in viewContext: NSManagedObjectContext) throws {
-    let currentUser = getCurrentUser(in: viewContext)
-    currentUser?.lastSync = Date.now
-    try viewContext.save()
-  }
-  
+
   static func getUserLastSync(id: Int, in viewContext: NSManagedObjectContext) throws -> Date? {
     let user = getUser(with: id, in: viewContext)
     return user?.lastSync
   }
-  
-  static func getCurrentUserId(in viewContext: NSManagedObjectContext) -> Int? {
-    guard let int32 = TogglUser.getCurrentUser(in: viewContext)?.id else { return nil }
-    return Int(int32)
-  }
-  
-  static func getCurrentUser(in viewContext: NSManagedObjectContext) -> TogglUser? {
-    guard
-      let currentUserId = keychain.get(.currentTogglUserId)?.toInt
-    else { return nil }
-    
-    return TogglUser.getUser(with: currentUserId, in: viewContext)
-  }
-  
-  static func getCurrentUserLastSync(in viewContext: NSManagedObjectContext) -> Date? {
-    guard
-    let currentUserId = keychain.get(.currentTogglUserId)?.toInt
-    else { return nil }
-    
-    return TogglUser.getLastSyncDate(for: currentUserId, in: viewContext)
-  }
-
   
   static func getUser
   (with id: Int, in viewContext: NSManagedObjectContext) -> TogglUser? {
